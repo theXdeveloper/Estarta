44adb101a4459977e22fc498381aaaa0b3c822f2
Sawsan Almahmoud <gsawsan.1988@gmail.com>
2023-01-14 14:57:14 +0300

    Initial Commit

---
+++ EstartaTask/EstartaTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstartaTaskApp.swift
+//  EstartaTask
+//
+//  Created by Sawsan Alkhdair on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct EstartaTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
